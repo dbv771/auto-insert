0a77a1ff82ff4917ad96507c169363a8bbef3db1
iory <ab.ioryz@gmail.com>
2017-01-06 04:56:56 +0900

    Add readme and packages.el

---
+++ packages.el
@@ -0,0 +1,41 @@
+;;; packages.el --- auto-insert layer packages file for Spacemacs.
+;;
+;; Author: iory <ab.ioryz@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; auto-insert-mode to enable spacemacs
+
+;;; Code:
+
+
+;;; Constants =================================================================
+
+(defconst auto-insert-layer-source-dir
+  (if load-file-name
+      (file-name-directory load-file-name)
+    default-directory)
+  "Source dir of auto-insert-layer")
+
+(defconst auto-insert-packages
+  '(auto-insert-mode)
+  )
+
+;; auto insertion
+(auto-insert-mode)
+(setq auto-insert-directory (concat auto-insert-layer-source-dir "/template"))
+(define-auto-insert "CMakeLists.txt" "cmake-template.txt")
+(define-auto-insert "\\.c$" "c-template.c")
+(define-auto-insert "\\.cpp$" "c-plusplus-template.cpp")
+(define-auto-insert "\\.l$" "euslisp-template.l")
+(define-auto-insert "\\.launch$" "launch-template.launch")
+(define-auto-insert "\\.py$" "py-template.py")
+(define-auto-insert "\\.sh$" "sh-template.sh")
+
+
+;;; packages.el ends here
